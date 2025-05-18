5fc6a22af60c189cd6f86fe56bff1b10c6dbff32
Raymond Paras <rayparas98@gmail.com>
2025-05-18 12:02:26 -0400

    Revert "Remove PersonStatusChecker class and just keep get_status as a function"
    This reverts commit 2e813b19fbc52398a5d0930c6930ca44825ffaeb.

--- schedule_builder/helpers/person_status_checker.py
+++ schedule_builder/helpers/person_status_checker.py
@@ -11,38 +11,44 @@ from schedule_builder.util.assignment_checker import (
 )
 
 
-def get_person_status(person: Person, check_date: date) -> PersonStatus:
+class PersonStatusChecker:
     """
-    Returns the status of a team member on a specific date.
-
-    Args:
-        person (Person): The team member whose status is being checked.
-        check_date (date): The date for which to check the status.
-
-    Returns:
-        PersonStatus: The status of the team member on the given date.
+    Utility class to check the status of a team member on a given date.
     """
-    if person.on_leave:
-        return PersonStatus.ONLEAVE
 
-    if check_date in person.blockout_dates:
-        return PersonStatus.BLOCKEDOUT
+    @staticmethod
+    def get_status(person: Person, check_date: date) -> PersonStatus:
+        """
+        Returns the status of a team member on a specific date.
 
-    if check_date in person.assigned_dates:
-        return PersonStatus.ASSIGNED
+        Args:
+            person (Person): The team member whose status is being checked.
+            check_date (date): The date for which to check the status.
 
-    if check_date in person.preaching_dates:
-        return PersonStatus.PREACHING
+        Returns:
+            PersonStatus: The status of the team member on the given date.
+        """
+        if person.on_leave:
+            return PersonStatus.ONLEAVE
 
-    if has_exceeded_consecutive_assignments(
-        assigned_dates=person.assigned_dates,
-        preaching_dates=person.preaching_dates,
-        reference_date=check_date,
-        limit=CONSECUTIVE_ASSIGNMENTS_LIMIT,
-    ):
-        return PersonStatus.BREAK
+        if check_date in person.blockout_dates:
+            return PersonStatus.BLOCKEDOUT
 
-    if Role.WORSHIPLEADER in person.roles and check_date in person.teaching_dates:
-        return PersonStatus.TEACHING
+        if check_date in person.assigned_dates:
+            return PersonStatus.ASSIGNED
 
-    return PersonStatus.UNASSIGNED
+        if check_date in person.preaching_dates:
+            return PersonStatus.PREACHING
+
+        if has_exceeded_consecutive_assignments(
+            assigned_dates=person.assigned_dates,
+            preaching_dates=person.preaching_dates,
+            reference_date=check_date,
+            limit=CONSECUTIVE_ASSIGNMENTS_LIMIT,
+        ):
+            return PersonStatus.BREAK
+
+        if Role.WORSHIPLEADER in person.roles and check_date in person.teaching_dates:
+            return PersonStatus.TEACHING
+
+        return PersonStatus.UNASSIGNED
